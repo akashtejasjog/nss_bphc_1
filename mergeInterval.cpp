53b7a3a2861c8bc5d02e4000954003bd5c085d5c
atj <akash.tejas.jog@gmail.com>
2025-09-19 19:15:31 +0530

    Merge Intervals

---
+++ mergeInterval.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> out_vector;
+        for (auto& interval: intervals){
+            if (out_vector.empty()) {
+                out_vector.push_back(interval);
+            } else if (out_vector.back()[1] < interval[0]){
+                out_vector.push_back(interval);
+            } else {
+                out_vector.back()[1] = max(out_vector.back()[1], interval[1]);
+            }
+
+        }
+        return out_vector;
+    }
+};
